61679a0954f573dc5b2b69d39555662109e2bc13
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2024-08-04 03:18:56 +0530

    intial commit

---
+++ src/app/products/[productId]/page.jsx
@@ -0,0 +1,7 @@
+export default function ProductPage({ params }) {
+  return (
+    <div>
+      <h1>Product Details {params.productId}</h1>
+    </div>
+  );
+}
---
+++ src/app/products/page.jsx
@@ -0,0 +1,10 @@
+export default function products() {
+  return (
+    <div>
+      <h1>Products 1</h1>
+      <h1>Products 2</h1>
+      <h1>Products 3</h1>
+      <h1>Products 4</h1>
+    </div>
+  );
+}
