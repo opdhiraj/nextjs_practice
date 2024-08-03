f8e26cbce04ba1f08ac8ca9fb86ddaef87ead1d6
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2024-08-04 03:21:02 +0530

    test changes

--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -2,6 +2,7 @@ export default function ProductPage({ params }) {
   return (
     <div>
       <h1>Product Details {params.productId}</h1>
+      <h1>Product Details {params.productId}</h1>
     </div>
   );
 }
